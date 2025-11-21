0082b4b04b5276b87d2921cd6f505811afd6b165
mark777 <markgonzalez20062@gmail.com>
2025-11-20 23:10:28 -0500

    Update Main.java

--- src/main/java/ed/u2/Main.java
+++ src/main/java/ed/u2/Main.java
@@ -6,11 +6,13 @@ import ed.u2.benchmark.Benchmark;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Iniciando generación de datasets...");
+        //Crea datasets procesados para las pruebas y genera 4 archivos CSV diferentes.
         GeneradorDatasets gen = new GeneradorDatasets();
         gen.generarTodos(); // genera los 4 CSV en resources/datasets/
         System.out.println("Datasets generados en: " + Config.RUTA_DATASETS);
 
         System.out.println("\nIniciando benchmarks...");
+        //Evalua y usa los datasets generados previamente.
         Benchmark benchmark = new Benchmark();
         benchmark.ejecutarTodos(); // carga y evalúa los 3 algoritmos sobre los datasets
         System.out.println("Benchmark finalizado.");
