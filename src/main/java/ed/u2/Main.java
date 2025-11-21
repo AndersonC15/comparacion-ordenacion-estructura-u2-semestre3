87501e355db66fd1ccf162b8b16b25e27b43bf30
mark777 <markgonzalez20062@gmail.com>
2025-11-20 22:45:59 -0500

    Update Main.java

--- src/main/java/ed/u2/Main.java
+++ src/main/java/ed/u2/Main.java
@@ -1,17 +1,18 @@
 package ed.u2;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import ed.u2.generador.GeneradorDatasets;
+import ed.u2.benchmark.Benchmark;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Iniciando generación de datasets...");
+        GeneradorDatasets gen = new GeneradorDatasets();
+        gen.generarTodos(); // genera los 4 CSV en resources/datasets/
+        System.out.println("Datasets generados en: " + Config.RUTA_DATASETS);
+
+        System.out.println("\nIniciando benchmarks...");
+        Benchmark benchmark = new Benchmark();
+        benchmark.ejecutarTodos(); // carga y evalúa los 3 algoritmos sobre los datasets
+        System.out.println("Benchmark finalizado.");
     }
 }
